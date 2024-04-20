1b6d884b67920f7be0e65ca0e3e79f1ac3bcfd79
Dimonch1k <leskiv-dmytro@lyc2.org>
2024-04-20 22:01:19 +0300

    Rewrite code using BEM and update burger menu

--- src/js/google-map.js
+++ src/js/google-map.js
@@ -1,10 +1,10 @@
 function myMap() {
   alert = function () { }
-  var Center = new google.maps.LatLng(49.347959, 23.520748);
-  var Canvas = document.getElementById("map");
-  var Options = { center: Center, zoom: 5 };
-  var map = new google.maps.Map(Canvas, Options);
-  var marker = new google.maps.Marker({ position: Center });
+  let Center = new google.maps.LatLng(49.347959, 23.520748);
+  let Canvas = document.getElementById("map");
+  let Options = { center: Center, zoom: 5 };
+  let map = new google.maps.Map(Canvas, Options);
+  let marker = new google.maps.Marker({ position: Center });
   marker.setMap(map);
   google.maps.event.addListener(marker, 'click', function () {
     map.setZoom(15);
